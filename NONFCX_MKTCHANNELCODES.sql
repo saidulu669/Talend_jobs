97d88e264c49105e8373e21057fa783efce139ca
saidulu669 <83533241+saidulu669@users.noreply.github.com>
2021-05-02 14:58:50 +0530

    Add files via upload

---
+++ NONFCX_MKTCHANNELCODES.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS TDL.NONFCX_mktchannelcodes
+(
+	channel_code VARCHAR(16)   
+	,global_marketing_channel VARCHAR(50)   
+)
+
+;
---
+++ NONFCX_SUBSCRIPTIONSTARTDATA.sql
@@ -0,0 +1,6 @@
+create or replace table TDL.NONFCX_SubscriptionStartdata (jrc	varchar(200),
+startdate	date,
+marketingchannel	varchar(100),
+businessfunction	varchar(100),
+promcode	varchar(50),
+subscriptionnumber integer);
